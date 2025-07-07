231ea7d9cce1da10195da82717e8b1fb9ab83af0
aaggarwal <aaggarwal@alpha-sense.com>
2025-07-07 14:43:11 +0530

    FML

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import NovelsPage from "./pages/NovelsPage";
 import ChaptersPage from "./pages/ChaptersPage";
 import ChapterContentPage from "./pages/ChapterContentPage";
 import Navigation from "./components/Navigation";
-import SimpleAudioPlayerDemo from "./components/SimpleAudioPlayerDemo";
 import { getCurrentUsername } from "./utils/config";
 import "./styles/globals.css";
 import "./styles/components.css";
@@ -40,7 +39,6 @@ const App: React.FC = () => {
         <div className="relative z-10">
           <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/simple-player-demo" element={<SimpleAudioPlayerDemo />} />
             <Route
               path="/novels"
               element={
